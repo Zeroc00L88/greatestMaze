835caeddd89c090a05e0bf9afc797d23a543393d
Jerome <jeromelesne@gmail.com>
2024-01-25 00:35:45 +0100

    check around function implemented

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="./assets/css/style.css" rel="stylesheet" />
+</head>
+
+
 <?php
 
 echo "<body>";
-echo "<h1>THE GREATEST MAZE</h1>";
+echo "<header><h1>THE GREATEST MAZE</h1></header>";
+echo "<main>";
 // W = wall; F = fog; C = Cat; E = Empty; M = Mouse
 $arrOne = [
-    ["E", "W", "E", "E", "E", "W"],
-    ["E", "W", "E", "W", "W", "W"],
+    ["C", "W", "E", "E", "E", "W"],
+    ["F", "W", "E", "W", "W", "W"],
     ["E", "E", "E", "W", "M", "W"],
     ["E", "W", "E", "W", "E", "E"],
     ["E", "W", "E", "E", "W", "E"],
@@ -23,11 +35,15 @@ $arrTwo = [
 
 echo "<div id='mazeContainer'>";
 
-function drawMaze($array){
-    foreach ($array as $row) {
-        echo "<div class='row'>";
-        foreach($row as $cell){
-            echo "<div class='cell' width='96' height='96'>";
+function console_log($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+function showCell($cell){
             switch ($cell) {
                 case 'W':
                     echo "<img src='./assets/images/wall.png' width='96'>";
@@ -45,11 +61,32 @@ function drawMaze($array){
                     echo "";
                     break;
             }
+}
+
+function cellsAround($array, $x, $y){
+    $cellsArd = [];
+    $y != 0 ? $cellsArd["up"] = $array[$y-1][$x] : $cellsArd["up"] = "out";
+    $x != count($array[$y])-1 ? $cellsArd["right"]= $array[$y][$x+1] : $cellsArd["right"] = "out";
+    $y != count($array)-1 ? $cellsArd["down"] = $array[$y+1][$x] : $cellsArd["down"] = "out";
+    $x != 0 ? $cellsArd["left"] = $array[$y][$x-1] : $cellsArd["left"] = "out";
+    return $cellsArd;
+}
+
+function drawMaze($array){
+    foreach ($array as $x => $row) {
+        echo "<div class='row'>";
+        foreach($row as $y => $cell){
+            echo "<div class='cell' width='96' height='96'>";
+            if ($cell === "C"){
+                console_log(cellsAround($array, $x, $y));
+            }
+            showCell($cell);
             echo "</div>";
         }
         echo "</div>";
     }
 }
+
 
 drawMaze($arrOne);
 
@@ -57,31 +94,20 @@ echo "</div>";
 echo "<div id='arrowContainer'>";
 echo "
 <form method='POST'>
+    <div>
+        <input type='submit' name='up' value='up'>
+    </div>
+    <div>
+        <input type='submit' name='left' value='left'>
+        <input type='submit' name='right' value='right'>
+    </div>
+    <div>
+        <input type='submit' name='down' value='down'>
+    </div>
 </form>
 ";
 echo "</div>";
 
-echo "
-<style>
-    h1 {
-        display: flex;
-        justify-content: center;
-    }
-    .row {
-        display: flex;
-    }
-    .cell {
-        width: 96px;
-    }
-    #mazeContainer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center; 
-        border: 1px solid black;
-        max-width: 100%;
-    }
-</style>
-";
+echo "</main>";
 echo "</body>";
 ?>
