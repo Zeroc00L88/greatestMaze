3818cd4e7c73db681207439c4f7cc958a217e4d0
Jerome <jeromelesne@gmail.com>
2024-01-29 13:15:21 +0100

    implemented random maze generation

--- index.php
+++ index.php
@@ -1,3 +1,186 @@
+<?php
+
+session_start();
+
+// W = wall; F = fog; C = Cat; E = Empty; M = Mouse
+$displayedArr = [
+    ["F", "F", "F", "F", "F", "F"],
+    ["F", "F", "F", "F", "F", "F"],
+    ["F", "F", "F", "F", "F", "F"],
+    ["F", "F", "F", "F", "F", "F"],
+    ["F", "F", "F", "F", "F", "F"],
+    ["F", "F", "F", "F", "F", "F"],
+];
+
+function randArrGen($min, $max)
+{
+    // Generate the size of the map
+    $arrX = rand($min, $max);
+    $arrY = rand($min, $max);
+    // Fill the map
+    $arr = array_fill(0, $arrY, array_fill(0, $arrX, "W"));
+    // Add mouse
+    $arr[rand(0, $arrY - 1)][rand(0, $arrX - 1)] = "M";
+    // Add cat check that the rand position is not Mouse
+    $x = rand(0, $arrX - 1);
+    $y = rand(0, $arrY - 1);
+
+    while ($arr[$y][$x] === "M") {
+        $x = rand(0, $arrX - 1);
+        $y = rand(0, $arrY - 1);
+    }
+    $xCat = $x;
+    $yCat = $y;
+    $arr[$yCat][$xCat] = "E";
+
+    while (true) {
+        $randMove = rand(1, 4);
+        if($y != 0 && $randMove == 1) {
+            if($arr[$y - 1][$x] == "M") {
+                return [$arr, $arrX, $arrY, $xCat, $yCat];
+            }
+            $arr[$y - 1][$x] =  "E";
+            $y--;
+        }
+        if($x != count($arr[$y]) - 1 && $randMove == 2) {
+            if($arr[$y][$x + 1] == "M") {
+                return [$arr, $arrX, $arrY, $xCat, $yCat];
+            }
+            $arr[$y][$x + 1] = "E";
+            $x++;
+        }
+        if($y != count($arr) - 1 && $randMove == 3) {
+            if ($arr[$y + 1][$x] == "M") {
+                return [$arr, $arrX, $arrY, $xCat, $yCat];
+            }
+            $arr[$y + 1][$x] = "E";
+            $y++;
+        }
+        if($x != 0 && $randMove == 4) {
+            if ($arr[$y][$x - 1] == "M") {
+                return [$arr, $arrX, $arrY, $xCat, $yCat];
+            }
+            $arr[$y][$x - 1] = "E";
+            $x--;
+        }
+    }
+}
+
+// function randArr()
+// {
+//     $arrs = [
+//         [
+//             ["E", "W", "E", "E", "E", "E"],
+//             ["E", "W", "E", "W", "W", "W"],
+//             ["E", "E", "E", "W", "M", "W"],
+//             ["E", "W", "E", "W", "E", "E"],
+//             ["E", "W", "E", "E", "W", "E"],
+//             ["E", "E", "W", "E", "E", "E"],
+//         ],
+//         [
+//             ["E", "E", "E", "E", "E", "W"],
+//             ["W", "W", "E", "W", "W", "W"],
+//             ["E", "E", "E", "W", "M", "E"],
+//             ["E", "W", "W", "W", "W", "E"],
+//             ["E", "W", "E", "E", "E", "E"],
+//             ["E", "E", "E", "W", "W", "W"],
+//         ],
+//     ];
+//     return $arrs[rand(0, 1)];
+// }
+
+function showCell($cell)
+{
+    switch ($cell) {
+        case 'W':
+            echo "<img src='./assets/images/wall.png' width='96'>";
+            break;
+        case 'F':
+            echo "<img src='./assets/images/fog.png' width='96'>";
+            break;
+        case 'C':
+            echo "<img src='./assets/images/cat.png' width='96'>";
+            break;
+        case 'M':
+            echo "<img src='./assets/images/mouse.png' width='96'>";
+            break;
+        case 'E':
+            echo "";
+            break;
+    }
+}
+
+function enlight($array, $arrayDisp, $catPos)
+{
+    $x = $catPos[0];
+    $y = $catPos[1];
+    $arrayDisp[$y][$x] = "C";
+    if($y != 0) {
+        $arrayDisp[$y - 1][$x] = $array[$y - 1][$x];
+    }
+    if($x != count($array[$y]) - 1) {
+        $arrayDisp[$y][$x + 1] = $array[$y][$x + 1];
+    }
+    if($y != count($array) - 1) {
+        $arrayDisp[$y + 1][$x] = $array[$y + 1][$x];
+    }
+    if($x != 0) {
+        $arrayDisp[$y][$x - 1] = $array[$y][$x - 1];
+    }
+    return $arrayDisp;
+}
+
+function drawMaze($array)
+{
+    echo "<div id='mazeContainer'>";
+    foreach ($array as $y => $row) {
+        echo "<div class='row'>";
+        foreach($row as $x => $cell) {
+            echo "<div class='cell' width='96' height='96'>";
+            showCell($cell);
+            echo "</div>";
+        }
+        echo "</div>";
+    }
+    echo "</div>";
+}
+
+function move($arr, $arrDisp, $catPos)
+{
+    $x = $catPos[0];
+    $y = $catPos[1];
+    if(isset($_POST["up"])) {
+        if($y != 0 && $arr[$y - 1][$x] != "W") {
+            $arrDisp[$y - 1][$x] = "C";
+            $_SESSION["catPos"][1]--;
+            return $arrDisp;
+        }
+    }
+    if(isset($_POST["right"])) {
+        if($x != count($arr[$y]) - 1 && $arr[$y][$x + 1] != "W") {
+            $arrDisp[$y][$x + 1] = "C";
+            $_SESSION["catPos"][0]++;
+            return $arrDisp;
+        }
+    };
+    if(isset($_POST["down"])) {
+        if($y != count($arr) - 1 && $arr[$y + 1][$x] != "W") {
+            $arrDisp[$y + 1][$x] = "C";
+            $_SESSION["catPos"][1]++;
+            return $arrDisp;
+        }
+    };
+    if(isset($_POST["left"])) {
+        if($x != 0 && $arr[$y][$x - 1] != "W") {
+            $arrDisp[$y][$x - 1] = "C";
+            $_SESSION["catPos"][0]--;
+            return $arrDisp;
+        }
+    }
+    return $arrDisp;
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -11,168 +194,43 @@
 
 <body>
     <header>
-        <h1>THE GREATEST MAZE</h1>
+    <h1>THE GREATEST MAZE</h1>
     </header>
 <main>
 
 <?php
-    session_start();
 
-    // W = wall; F = fog; C = Cat; E = Empty; M = Mouse
-    $displayedArr = [
-        ["F", "F", "F", "F", "F", "F"],
-        ["F", "F", "F", "F", "F", "F"],
-        ["F", "F", "F", "F", "F", "F"],
-        ["F", "F", "F", "F", "F", "F"],
-        ["F", "F", "F", "F", "F", "F"],
-        ["F", "F", "F", "F", "F", "F"],
-    ];
+//Reloading button
+if (isset($_POST["reload"])) {
+    session_destroy();
+    header("refresh:0");
+}
+//Chose a random maze
+if (!isset($_SESSION["maze"])) {
+    $randGen = randArrGen(4, 6);
+    $_SESSION["maze"] = $randGen[0];
+    $_SESSION["arrX"] = $randGen[1];
+    $_SESSION["arrY"] = $randGen[2];
+}
 
-    function randArrGen($x, $y, $insert){
-        $arr = array_fill(0, $y, array_fill(0, $x, $insert));
-        return $arr;
-    }
+$fogArr = array_fill(0, $_SESSION["arrY"], array_fill(0, $_SESSION["arrX"], "F"));
 
-    function pathFinder($min, $max, $insert){
-        $arrX = rand($min, $max);
-        $arrY = rand($min, $max);
-        $arr = randArrGen($arrX, $arrY, $insert);
-        $arr[rand(0, $arrY-1)][rand(0, $arrX-1)] = "C";
-        $x = rand(0, $arrX-1);
-        $y = rand(0, $arrY-1);
-        while ($arr[$y][$x] === "C") {
-            $x = rand(0, $arrX-1);
-            $y = rand(0, $arrY-1);
-        }
-        $arr[$y][$x] = "M";
-        print_r($arr);
-    }
+// Placing cat for first launch
+if (!isset($_SESSION["catPos"])) {
+    $_SESSION["catPos"] = [];
+    $_SESSION["catPos"][0] = $randGen[3];
+    $_SESSION["catPos"][1] = $randGen[4];
+    //    $fogArr[$_SESSION["catPos"][0]][$_SESSION["catPos"][1]] = "C";
+}
 
-    function randArr(){
-        $arrs = [
-            [
-                ["E", "W", "E", "E", "E", "E"],
-                ["E", "W", "E", "W", "W", "W"],
-                ["E", "E", "E", "W", "M", "W"],
-                ["E", "W", "E", "W", "E", "E"],
-                ["E", "W", "E", "E", "W", "E"],
-                ["E", "E", "W", "E", "E", "E"],
-            ],
-            [
-                ["E", "E", "E", "E", "E", "W"],
-                ["W", "W", "E", "W", "W", "W"],
-                ["E", "E", "E", "W", "M", "E"],
-                ["E", "W", "W", "W", "W", "E"],
-                ["E", "W", "E", "E", "E", "E"],
-                ["E", "E", "E", "W", "W", "W"],
-            ],
-        ];
-        return $arrs[rand(0,1)];
-    }
+// Checking move input
+$fogArr = move($_SESSION["maze"], $fogArr, $_SESSION["catPos"]);
+// Removing fog
+$fogArr = enlight($_SESSION["maze"], $fogArr, $_SESSION["catPos"]);
+// Draw the maze
+drawMaze($fogArr);
 
-    function showCell($cell){
-        switch ($cell) {
-            case 'W':
-                echo "<img src='./assets/images/wall.png' width='96'>";
-                break;
-            case 'F':
-                echo "<img src='./assets/images/fog.png' width='96'>";
-                break;
-            case 'C':
-                echo "<img src='./assets/images/cat.png' width='96'>";
-                break;
-            case 'M':
-                echo "<img src='./assets/images/mouse.png' width='96'>";
-                break;
-            case 'E':
-                echo "";
-                break;
-        }
-    }
-
-    function enlight($array, $arrayDisp, $catPos){
-        $x = $catPos[0];
-        $y = $catPos[1];
-        $arrayDisp[$y][$x] = "C";
-        if($y != 0) $arrayDisp[$y-1][$x] = $array[$y-1][$x];
-        if($x != count($array[$y])-1) $arrayDisp[$y][$x+1] = $array[$y][$x+1];
-        if($y != count($array)-1) $arrayDisp[$y+1][$x] = $array[$y+1][$x];
-        if($x != 0) $arrayDisp[$y][$x-1] = $array[$y][$x-1];
-        return $arrayDisp;
-    }
-
-    function drawMaze($array){
-        echo "<div id='mazeContainer'>";
-            foreach ($array as $y => $row) {
-                echo "<div class='row'>";
-                foreach($row as $x => $cell){
-                    echo "<div class='cell' width='96' height='96'>";
-                    showCell($cell);
-                    echo "</div>";
-                }
-                echo "</div>";
-            }
-        echo "</div>";
-    }
-
-    function move($arr, $arrDisp, $catPos){
-        $x = $catPos[0];
-        $y = $catPos[1];
-        if(isset($_POST["up"])) {
-            if($y != 0 && $arr[$y-1][$x] != "W"){
-                $arrDisp[$y-1][$x] = "C";
-                $_SESSION["catPos"][1]--;
-                return $arrDisp;
-            } 
-        }
-        if(isset($_POST["right"])){
-            if($x != count($arr[$y]) - 1 && $arr[$y][$x+1] !="W"){
-                $arrDisp[$y][$x+1] = "C";
-                $_SESSION["catPos"][0]++;
-                return $arrDisp;
-            } 
-        };
-        if(isset($_POST["down"])) {
-            if($y != count($arr) - 1 && $arr[$y+1][$x] != "W"){
-                $arrDisp[$y+1][$x] = "C";
-                $_SESSION["catPos"][1]++;
-                return $arrDisp;
-            } 
-        };
-        if(isset($_POST["left"])){
-            if($x != 0 && $arr[$y][$x-1] != "W"){
-                $arrDisp[$y][$x-1] = "C";
-                $_SESSION["catPos"][0]--;
-                return $arrDisp;
-            } 
-        }
-        return $arrDisp;
-    }
-    //Reloading button
-    if (isset($_POST["reload"])){
-        session_destroy();
-        header("refresh:0");
-    }
-    //Chose a random maze
-    if (!isset($_SESSION["maze"])){
-        $_SESSION["maze"] = randArr();
-    }
-
-    // Placing cat for first launch
-    if (!isset($_SESSION["catPos"])) {
-        $_SESSION["catPos"] = [0, 0];
-        $displayedArr[$_SESSION["catPos"][0]][$_SESSION["catPos"][1]] = "C";
-    }
-
-    // Checking move input
-    $displayedArr = move($_SESSION["maze"], $displayedArr, $_SESSION["catPos"]);
-    // Removing fog
-    $displayedArr = enlight($_SESSION["maze"], $displayedArr, $_SESSION["catPos"]);
-    // Draw the maze
-    drawMaze($displayedArr);
-    pathFinder(5, 10, "F");
 ?>
-
 
 <form method='POST'>
     <div id='arrowContainer'>
